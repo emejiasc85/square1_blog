7200c099221262be8ff3bdf83a31194a2ae75b93
Enrique Mejias <emejiasc85@gmail.com>
2021-04-30 11:20:54 -0600

    instal and config laravel breeze

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Admin',
+            'email'     => 'admin@client.com',
+            'password'  => Hash::make('password'),
+        ]);
+    }
+}
