c62822d14099354631d65cce0b870065a1822449
Enrique Mejias <emejiasc85@gmail.com>
2021-04-30 12:44:37 -0600

    add meta api-token

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="api-token" content="{{ auth()->user() ? auth()->user()->api_token : null }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
