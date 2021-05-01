d8c5ff8049181d051d3f59be26941f4822155613
Enrique Mejias <emejiasc85@gmail.com>
2021-04-30 23:56:56 -0600

    remove slug parameter

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
         return PostResource::collection($posts);
     }
     
-    public function show(Post $post, $slug)
+    public function show(Post $post)
     {
         return new PostResource($post);
     }
