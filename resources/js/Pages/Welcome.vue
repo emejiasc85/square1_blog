db7d3f000819ef03e3b7f8e8c53d8bedd294f3f6
Enrique Mejias <emejiasc85@gmail.com>
2021-05-01 13:31:30 -0600

    add search

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,8 +4,8 @@
             <div class="flex flex-col container mx-auto md:flex-row md:items-center md:justify-between">
                 <div class="flex justify-between items-center">
                     <div>
-                        <inertia-link href="/" class="text-gray-800 text-xl font-bold md:text-2xl">
-                            Square1 Blog
+                        <inertia-link href="/" class="text-gray-800 text-xl font-bold md:text-2xl flex">
+                            <img src="/images/logo.jpeg" class="flex-col" alt="square1"> <span class="flex-col pt-1 ml-2"> Blog</span>
                         </inertia-link>
                     </div>
                     <div>
@@ -39,7 +39,28 @@
                 <div class="w-full">
                     <div class="flex items-center justify-between">
                         <h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
-                        <div>
+                        
+                        <div class="flex">
+                            <div class="relative mx-auto text-gray-600 mr-2">
+                                <input 
+                                    v-model="search" 
+                                    class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
+                                    type="text" 
+                                    name="search" 
+                                    placeholder="Search"
+                                    ref="search"
+                                    @keyup.enter="index"
+                                >
+                                <button type="submit" class="absolute right-0 top-0 mt-3 mr-4" @click.prevent="index">
+                                    <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+                                        viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+                                        width="512px" height="512px">
+                                        <path
+                                        d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                                    </svg>
+                                </button>
+                            </div>
                             <select v-model="order" class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                 <option value="asc" selected>Latest</option>
                                 <option value="desc">Oldest</option>
