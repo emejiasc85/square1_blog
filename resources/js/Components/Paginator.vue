0eef34fac4f656c99b3da7ba113fdceb1f9667f7
Enrique Mejias <emejiasc85@gmail.com>
2021-04-30 20:01:11 -0600

    remove border

--- resources/js/Components/Paginator.vue
+++ resources/js/Components/Paginator.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+    <div class=" px-4 py-3 flex items-center justify-between sm:px-6">
         <div class="flex-1 flex justify-between sm:hidden">
             <a href="#" v-if="pagination.current_page > 1" @click.prevent="changePage(pagination.current_page - 1)" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                 Next
